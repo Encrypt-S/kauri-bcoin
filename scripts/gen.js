21ef1ea626696248e8ad5696a58556e3b7c77dd1
Craig <craig.b.macgregor@gmail.com>
2017-05-11 13:41:34 +1200

    test genesis generation

--- scripts/gen.js
+++ scripts/gen.js
@@ -10,6 +10,7 @@ var Script = require('../lib/script/script');
 var Opcode = require('../lib/script/opcode');
 var opcodes = Script.opcodes;
 var main, testnet, regtest, segnet3, segnet4, btcd;
+var mineGenesis = true;
 
 function createGenesisBlock(options) {
   var flags = options.flags;
@@ -19,7 +20,7 @@ function createGenesisBlock(options) {
 
   if (!flags) {
     flags = new Buffer(
-      'The Times 03/Jan/2009 Chancellor on brink of second bailout for banks',
+      'The Times 10/May/2017 Sacking FBI chief had nothing to do with Russia inquiry, says Trump',
       'ascii');
   }
 
@@ -72,73 +73,102 @@ function createGenesisBlock(options) {
   return block;
 }
 
-main = createGenesisBlock({
-  version: 1,
-  ts: 1231006505,
-  bits: 486604799,
-  nonce: 2083236893
-});
 
-testnet = createGenesisBlock({
-  version: 1,
-  ts: 1296688602,
-  bits: 486604799,
-  nonce: 414098458
-});
+if (mineGenesis) {
+  util.log('generating new genesis block');
+  var nonce = 0;
+  var hashGenesisBlock = 0x001;
+  var powLimit = new BN(
+    '00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+    'hex'
+  );
+  main = createGenesisBlock({
+    version: 1,
+    ts: 1494456197,
+    bits: 486604799,
+    nonce: nonce,
+  });
 
-regtest = createGenesisBlock({
-  version: 1,
-  ts: 1296688602,
-  bits: 545259519,
-  nonce: 2
-});
+  while (main.hash > powLimit) {
+    main = undefined;
+    main = createGenesisBlock({
+      version: 1,
+      ts: 1494456197,
+      bits: 486604799,
+      nonce: nonce,
+    });
+    // util.log('main hash: %s', main.rhash(), ' nonce: ', nonce);
+    nonce++;
+  }
+} else {
+  main = createGenesisBlock({
+    version: 1,
+    ts: 1494456197,
+    bits: 486604799,
+    nonce: 2083236893,
+  });
+}
 
-segnet3 = createGenesisBlock({
-  version: 1,
-  ts: 1452831101,
-  bits: 486604799,
-  nonce: 0
-});
-
-segnet4 = createGenesisBlock({
-  version: 1,
-  ts: 1452831101,
-  bits: 503447551,
-  nonce: 0
-});
-
-btcd = createGenesisBlock({
-  version: 1,
-  ts: 1401292357,
-  bits: 545259519,
-  nonce: 2
-});
+// testnet = createGenesisBlock({
+//   version: 1,
+//   ts: 1494456197,
+//   bits: 486604799,
+//   nonce: 414098458
+// });
+//
+// regtest = createGenesisBlock({
+//   version: 1,
+//   ts: 1494456197,
+//   bits: 545259519,
+//   nonce: 2
+// });
+//
+// segnet3 = createGenesisBlock({
+//   version: 1,
+//   ts: 1494456197,
+//   bits: 486604799,
+//   nonce: 0
+// });
+//
+// segnet4 = createGenesisBlock({
+//   version: 1,
+//   ts: 1494456197,
+//   bits: 503447551,
+//   nonce: 0
+// });
+//
+// btcd = createGenesisBlock({
+//   version: 1,
+//   ts: 1494456197,
+//   bits: 545259519,
+//   nonce: 2
+// });
 
 util.log(main);
 util.log('');
-util.log(testnet);
-util.log('');
-util.log(regtest);
-util.log('');
-util.log(segnet3);
-util.log('');
-util.log(segnet4);
-util.log('');
-util.log('');
+// util.log(testnet);
+// util.log('');
+// util.log(regtest);
+// util.log('');
+// util.log(segnet3);
+// util.log('');
+// util.log(segnet4);
+// util.log('');
+// util.log('');
 util.log('main hash: %s', main.rhash());
 util.log('main raw: %s', main.toRaw().toString('hex'));
 util.log('');
-util.log('testnet hash: %s', testnet.rhash());
-util.log('testnet raw: %s', testnet.toRaw().toString('hex'));
-util.log('');
-util.log('regtest hash: %s', regtest.rhash());
-util.log('regtest raw: %s', regtest.toRaw().toString('hex'));
-util.log('');
-util.log('segnet3 hash: %s', segnet3.rhash());
-util.log('segnet3 raw: %s', segnet3.toRaw().toString('hex'));
-util.log('');
-util.log('segnet4 hash: %s', segnet4.rhash());
-util.log('segnet4 raw: %s', segnet4.toRaw().toString('hex'));
-util.log('');
-util.log('btcd simnet hash: %s', btcd.rhash());
-util.log('btcd simnet raw: %s', btcd.toRaw().toString('hex'));
+// util.log('testnet hash: %s', testnet.rhash());
+// util.log('testnet raw: %s', testnet.toRaw().toString('hex'));
+// util.log('');
+// util.log('regtest hash: %s', regtest.rhash());
+// util.log('regtest raw: %s', regtest.toRaw().toString('hex'));
+// util.log('');
+// util.log('segnet3 hash: %s', segnet3.rhash());
+// util.log('segnet3 raw: %s', segnet3.toRaw().toString('hex'));
+// util.log('');
+// util.log('segnet4 hash: %s', segnet4.rhash());
+// util.log('segnet4 raw: %s', segnet4.toRaw().toString('hex'));
+// util.log('');
+// util.log('btcd simnet hash: %s', btcd.rhash());
+// util.log('btcd simnet raw: %s', btcd.toRaw().toString('hex'));
